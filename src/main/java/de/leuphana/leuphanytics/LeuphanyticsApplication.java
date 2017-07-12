8f3e7ee30523da11fc34f50a622b2c932bc22947
darkD3mon <jonasmail01@googlemail.com>
2017-07-12 12:00:04 +0200

    Intial Spring Boot Vaadin Setup

---
+++ src/main/java/de/leuphana/leuphanytics/LeuphanyticsApplication.java
@@ -0,0 +1,12 @@
+package de.leuphana.leuphanytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeuphanyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeuphanyticsApplication.class, args);
+	}
+}
