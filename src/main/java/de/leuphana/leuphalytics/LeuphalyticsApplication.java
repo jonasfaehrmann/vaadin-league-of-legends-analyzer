64eee23a391a9a79a8975e3e132b49a6b9701045
Bennett <dams.bennett@gmail.com>
2017-07-14 01:33:18 +0200

    added UI to component scan in main application

--- src/main/java/de/leuphana/leuphalytics/LeuphalyticsApplication.java
+++ src/main/java/de/leuphana/leuphalytics/LeuphalyticsApplication.java
@@ -13,19 +13,17 @@ import org.springframework.web.client.RestTemplate;
 import de.leuphana.leuphalytics.model.Dashboard;
 import de.leuphana.leuphalytics.model.user.ExternalAccountEnum;
 import de.leuphana.leuphalytics.model.user.User;
-import de.leuphana.leuphalytics.model.widget.MatchList;
 import de.leuphana.leuphalytics.model.widget.RiotMatchList;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "de.leuphana.leuphalytics.model", "de.leuphana.leuphalytics.view",
-		"de.leuphana.leuphalytics.controller" })
+@ComponentScan(basePackages = { "de.leuphana.leuphalytics", "de.leuphana.leuphalytics.model",
+		"de.leuphana.leuphalytics.view", "de.leuphana.leuphalytics.controller" })
 public class LeuphalyticsApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(LeuphalyticsApplication.class);
 
 	User user;
 	Dashboard dashboard;
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(LeuphalyticsApplication.class);
@@ -43,9 +41,11 @@ public class LeuphalyticsApplication {
 		ExternalAccountEnum accountType = ExternalAccountEnum.RIOT;
 
 		return args -> {
+
 			RiotMatchList riotMatchList = restTemplate.getForObject(
 					"https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/35211455?api_key=RGAPI-fac0c7a0-fe86-409a-9b5e-1682b6da3107",
 					RiotMatchList.class);
+
 			log.info(riotMatchList.toString());
 		};
 
