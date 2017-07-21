e91276ae520aaf2239894daadcb83968fe6936cd
darkD3mon <jonasmail01@googlemail.com>
2017-07-21 10:54:04 +0200

    added champion classes

---
+++ src/main/java/de/leuphana/leuphalytics/model/champion/Example.java
@@ -0,0 +1,35 @@
+
+package de.leuphana.leuphalytics.model.champion;
+
+
+public class Example {
+
+    private String type;
+    private String version;
+    private ChampionData data;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public ChampionData getData() {
+        return data;
+    }
+
+    public void setData(ChampionData data) {
+        this.data = data;
+    }
+
+}
