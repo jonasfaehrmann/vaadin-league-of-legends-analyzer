d8be5d9f9fb513c6ee8d891d593da0ed9c67aeb5
darkD3mon <jonasmail01@googlemail.com>
2017-08-07 20:15:33 +0200

    UI COMPONENT: Added DetailMatch View

--- src/main/java/de/leuphana/backend/service/RiotService.java
+++ src/main/java/de/leuphana/backend/service/RiotService.java
@@ -10,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 import net.rithms.riot.api.ApiConfig;
 import net.rithms.riot.api.RiotApi;
 import net.rithms.riot.api.RiotApiException;
+import net.rithms.riot.api.endpoints.match.dto.Match;
 import net.rithms.riot.api.endpoints.match.dto.MatchList;
 import net.rithms.riot.api.endpoints.match.dto.MatchReference;
 import net.rithms.riot.api.endpoints.static_data.dto.Champion;
@@ -42,6 +43,12 @@ public class RiotService {
 		Map<String, Champion> championMap = championList.getData();
 
 		return championMap;
+	}
+	
+	public Match getMatchById(long matchId) throws RiotApiException{
+		Match match = api.getMatch(Platform.NA, matchId);
+		
+		return match;
 	}
 	
 	// for testing
--- src/main/java/de/leuphana/ui/view/match/MatchDetailView.java
+++ src/main/java/de/leuphana/ui/view/match/MatchDetailView.java
@@ -4,19 +4,65 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.vaadin.data.BeanValidationBinder;
 import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
+
+import de.leuphana.backend.service.RiotService;
+import net.rithms.riot.api.endpoints.match.dto.Match;
+import net.rithms.riot.api.endpoints.match.dto.MatchReference;
+import net.rithms.riot.api.endpoints.match.dto.Participant;
+import net.rithms.riot.api.endpoints.match.dto.ParticipantStats;
 
 @SpringView(name = "match")
 public class MatchDetailView extends MatchDetailViewDesign implements View {
-	
+
+	private MatchDetailView view;
+	private BeanValidationBinder<Match> binder;
+
 	@Autowired
-	public MatchDetailView(){
+	private RiotService riotService;
+
+	@Autowired
+	public MatchDetailView() {
 	}
 
 	@PostConstruct
 	public void init() {
+
+	}
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		String matchId = event.getParameters();
+		if ("".equals(matchId)) {
+			System.out.println("No Match Id");
+		} else {
+			Match match;
+			try {
+				match = riotService.getMatchById(Long.valueOf(matchId));
+			} catch (Exception e) {
+				view.showNotFound();
+				return;
+			}
+			setMatch(match);
+		}
+	}
+
+	public void setMatch(Match match) {
 		
+		gameId.setValue(String.valueOf(match.getGameId()));
+		
+		matchParticipantGrid.setSizeFull();
+		matchParticipantGrid.setCaption("Participants: ");
+		matchParticipantGrid.setItems(match.getParticipants());
+	}
+
+	public void showNotFound() {
+		removeAllComponents();
+		addComponent(new Label("Match not found"));
 	}
 }
