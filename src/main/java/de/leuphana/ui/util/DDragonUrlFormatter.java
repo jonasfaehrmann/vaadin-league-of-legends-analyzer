a95771b829370e18e0c13cc9f2e424b28f12405b
njage <njage@141.39.213.45>
2017-09-06 23:21:21 +0200

    update ItemImageImplementation

--- src/main/java/de/leuphana/ui/util/DDragonUrlFormatter.java
+++ src/main/java/de/leuphana/ui/util/DDragonUrlFormatter.java
@@ -3,10 +3,9 @@ package de.leuphana.ui.util;
 public class DDragonUrlFormatter {
 
 	private static final String baseUrl = "http://ddragon.leagueoflegends.com/cdn/7.17.2/img/";
-private static final String baseUrlItem ="http://ddragon.leagueoflegends.com/cdn/7.17.2/img/";
-	
+
 	public String getUrlbyItemImageName(String imgName) {
-		return baseUrlItem+"item/"+imgName;
+		return baseUrl+"item/"+imgName;
 	}
 	
 	
