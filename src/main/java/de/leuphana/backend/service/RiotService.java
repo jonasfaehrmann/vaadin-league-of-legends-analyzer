a740f96a17562abe11a1f5a4be051b8caf7a5ae5
Bennett Dams <dams.bennett@gmail.com>
2017-09-17 01:02:21 +0200

    implementation of session for locale variable

--- src/main/java/de/leuphana/backend/service/RiotService.java
+++ src/main/java/de/leuphana/backend/service/RiotService.java
@@ -1,17 +1,16 @@
 package de.leuphana.backend.service;
 
-import java.util.List;
-import java.util.stream.Stream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
 
+import com.vaadin.server.VaadinService;
 import com.vaadin.server.VaadinSession;
+import com.vaadin.server.WrappedSession;
 
 import net.rithms.riot.api.ApiConfig;
 import net.rithms.riot.api.RiotApi;
-import net.rithms.riot.api.RiotApiException;
-import net.rithms.riot.api.endpoints.match.dto.Match;
 import net.rithms.riot.api.endpoints.static_data.constant.Locale;
 import net.rithms.riot.constant.Platform;
 
@@ -24,16 +23,31 @@ public abstract class RiotService<T> {
 
 	protected RestTemplate restTemplate;
 	protected RiotApi api;
-	protected final ApiConfig config = new ApiConfig().setKey("RGAPI-1eda2a7c-add5-416f-8f0c-43fba60eafef");
+	protected final ApiConfig config = new ApiConfig().setKey("RGAPI-6bba33b3-e6cd-41c2-87fb-0cf7037f5105");
 	protected final Platform platform = Platform.EUW;
+	private static WrappedSession session;
 	private static Locale locale;
 
 	public static Locale getLocale() {
+
+		try {
+			session = VaadinSession.getCurrent().getSession();
+		} catch (Exception e) {
+			// e.printStackTrace();
+		}
+
+		if (session != null) {
+			if (locale == null) {
+				session.setAttribute("locale", Locale.DE_DE);
+			}
+			locale = (Locale) session.getAttribute("locale");
+		}
 		return locale;
 	}
 
 	public static void setLocale(Locale locale, String language) {
-		RiotService.locale = locale;
+		session = VaadinSession.getCurrent().getSession();
+		session.setAttribute("locale", locale);
 		System.out.println("Changed language to " + language);
 	}
 
